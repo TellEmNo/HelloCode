cca855da7ec017e73be8d708856e6c4f963b3f8d
Vladislav Verin <dippicaka@gmail.com>
2022-12-14 18:03:58 +0300

    add Example 005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "владислав")
+{
+	Console.WriteLine("Ура! это Влад зашёл!!!");
+}
+else
+{
+	Console.WriteLine("Фу, это не Влад!:(");
+	Console.Write("Ну, привет, ");
+	Console.WriteLine(username);
+}
