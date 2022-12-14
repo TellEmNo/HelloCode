fa053b108fb07bd873514a2f059a6e96386daa37
Vladislav Verin <dippicaka@gmail.com>
2022-12-14 18:29:47 +0300

    add Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = new Random().Next(0, 101);
+Console.WriteLine(a);
+
+int b = new Random().Next(0, 101);
+Console.WriteLine(b);
+
+int c = new Random().Next(0, 101);
+Console.WriteLine(c);
+
+int d = new Random().Next(0, 101);
+Console.WriteLine(d);
+
+int e = new Random().Next(0, 101);
+Console.WriteLine(e);
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Самая тяжелая гиря весит ");
+Console.Write(max);
+Console.Write(" кг");
