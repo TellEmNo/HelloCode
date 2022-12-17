7ad31c434705d2d7ee1aa90681a29a381f7d1e53
Vladislav Verin <dippicaka@gmail.com>
2022-12-17 16:54:03 +0300

    Method Array

---
+++ Example010MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] arr = { 11, 52, 36, 42, 534, 36, 47, 83, 92 };
+
+int n = arr.Length;
+Console.WriteLine("Введите искомое число");
+int f = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+	if (arr[i] == f)
+	{
+		Console.WriteLine(i);
+		break;
+	}
+}
