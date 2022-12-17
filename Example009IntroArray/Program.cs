1c2828f96a1359e77c9c963dc09c6e07fad18e64
Vladislav Verin <dippicaka@gmail.com>
2022-12-17 16:35:46 +0300

    Intro Array

---
+++ Example009IntroArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+	int result = arg1;
+	if (arg2 > result) result = arg2;
+	if (arg3 > result) result = arg3;
+	return result;
+}
+int max = 0;
+int[] arr = { 11, 32, 343, 24, 55, 76, 77, 683, 94, };
+
+for (int count = 0; count < arr.Length; count++)
+{
+	if (arr[count] > arr[max]) arr[max] = arr[count];
+}
+
+Console.WriteLine(arr[max]);
+
+
