846c118f3d8b034ed44ae9f9f75dac1d584a9a1b
Vladislav Verin <dippicaka@gmail.com>
2023-01-11 18:36:59 +0300

    Homework is done

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -94,6 +94,34 @@ Console.WriteLine();
 newText = Replace(newText, 'с', 'С');
 Console.WriteLine(newText);
  */
+/* 
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array){
+	int count = array.Length;
+	for (int i = 0; i < count; i++){
+		Console.Write($"{array[i]} ");
+	}
+	Console.WriteLine();
+}
+
+void SelectionSort(int[] array){
+	for (int i = 0; i < array.Length - 1; i++){
+		int minPosition = i;
+		for (int j = i + 1; j < array.Length; j++){
+			if(array[j]<array[minPosition]) minPosition = j;
+		}
+			int temporary = array[i];
+		array[i] = array[minPosition];
+		array[minPosition] = temporary;
+	}
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
+ */
 
 int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
 
@@ -107,13 +135,13 @@ void PrintArray(int[] array){
 
 void SelectionSort(int[] array){
 	for (int i = 0; i < array.Length - 1; i++){
-		int minPositioin = i;
+		int maxPosition = i;
 		for (int j = i + 1; j < array.Length; j++){
-			if(array[j]<array[minPositioin]) minPositioin = j;
+			if(array[j]>array[maxPosition]) maxPosition = j;
 		}
 			int temporary = array[i];
-		array[i] = array[minPositioin];
-		array[minPositioin] = temporary;
+		array[i] = array[maxPosition];
+		array[maxPosition] = temporary;
 	}
 }
 
